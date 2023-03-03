dfa8c06be810c4591a98a2704c8459cae48b5363
antoanstin <antoanstin@mail.com>
2023-03-03 09:46:54 +0530

    lki

---
+++ main.js
@@ -0,0 +1,199 @@
+// addition
+var a=3;
+var b=4;
+var c=a+b;
+console.log(c)
+
+// subrction
+var a=6;
+var b=4;
+var c=a-b;
+console.log(c)
+
+// multiplication
+var a=3;
+var b=4;
+var c=a*b;
+console.log(c)
+
+// divition
+var a=50;
+var b=5;
+var c=a/b;
+console.log(c)
+
+// modulus
+var a=50;
+var b=5;
+var c=a%b;
+console.log(c)
+
+// modulus with remainder
+var a=52;
+var b=5;
+var c=a%b;
+console.log(c)
+
+// comparisiom opertors
+// lessthan
+var a=50;
+var b=5;
+var c=a<b;
+console.log(c)
+
+// greaterthan
+var a=50;
+var b=5;
+var c=a>b;
+console.log(c)
+
+// lessthan or equalto
+var a=50;
+var b=5;
+var c=a<=b;
+console.log(c)
+
+// greaterthan or equalto
+var a=50;
+var b=5;
+var c=a>=b;
+console.log(c)
+
+// double equalto
+var a="7";
+var b=7;
+var c=a==b
+console.log(c)
+
+// not double equalto
+var a="7";
+var b=7;
+var c=a!=b
+console.log(c)
+
+// stict equalto
+var a="7";
+var b=7;
+var c=a===b
+console.log(c)
+
+// not stict equalto
+var a="7";
+var b=7;
+var c=a!==b
+console.log(c)
+
+// equalto
+var a=5;
+var b=a;
+console.log(b)
+
+// pre increament
+var a=9;
+var b=++a;
+console.log(b)
+
+// post increament
+var a=9;
+var b=a++;
+console.log(b)
+
+// pre decreament
+var a=9;
+var b=--a;
+console.log(b)
+
+// post decreament
+var a=9;
+var b=a--;
+console.log(b)
+ 
+ // BODMAS 
+var a=6;
+var b=9;
+var c=8;
+console.log(a%b*c)
+console.log(a*b*c)
+console.log(a-b+c)
+console.log(a%b/c)
+console.log(a-b%c)
+console.log(a%b*c)
+
+// logical operator
+var a=45;
+var b=19;
+console.log(a==b&&a<b)
+console.log(a==b||a<b)
+console.log(a==b!=a>b)
+
+// condition operator
+console.log("-------ifelse-------");
+ if(a>42)
+	{
+	 console.log("a is big");
+	}
+ else
+ { 
+	console.log ("a is not big");
+ }
+ console.log("--------ternery operator--------");
+ let f=b>23?"b is big":"b is not big";
+ console.log(f);
+ 
+ console.log("-------assignment operator-------");
+
+ 
+ var a=2; 
+ b=4;  
+ c=6; 
+ d=1; 
+ e=8; 
+ f=10;
+  console.log(b%a-d/c*f+e)
+  
+  var a=79;
+  b=a--;
+  console.log(a--);
+  console.log(++a);
+  console.log(--a);
+ 
+  var x=2;
+  y=4;
+  z=10;
+  console.log(x>y)&&(z<x)&&(y<Z)
+ 
+ // ternery operator
+var a=19;b=7;
+console.log(a>b? "eligible":"not eligible")
+
+	
+	
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
